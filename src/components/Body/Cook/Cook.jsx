23fb7d3b41f862d7b85dccdc11eda0d69bebd546
mahade <owork.hasan@gmail.com>
2025-12-29 23:32:02 +0600

    commited

--- src/components/Body/Cook/Cook.jsx
+++ src/components/Body/Cook/Cook.jsx
@@ -6,11 +6,22 @@ const Cook = ({ count, cookItems }) => {
     <div className="border-gray-400 border m-4 p-4 w-1/3 rounded-3xl">
       <h1 className="text-xl font-bold p-4 border-b">Want to cook: {count}</h1>
 
-      <div>
-        {cookItems.map((item, idx) => (
-          <CookItem key={idx} item={item}></CookItem>
-        ))}
-      </div>
+      <table className="w-full border-collapse mt-4">
+        <thead>
+          <tr className="border-b text-gray-400">
+            <th className="text-left text-sm py-2">Name</th>
+            <th className="text-left text-sm py-2">Time</th>
+            <th className="text-left text-sm py-2">Calories</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {cookItems.map((item, idx) => (
+            <CookItem key={idx} item={item} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/components/CookItem/CookItem.jsx
+++ src/components/CookItem/CookItem.jsx
@@ -1,7 +1,21 @@
 import PropTypes from "prop-types";
 
 const CookItem = ({ item }) => {
-  return <div>{item.recipe_id}</div>;
+  return (
+    <tr className="border-b">
+      <td className="text-sm py-3">{item.recipe_name}</td>
+
+      <td className="text-sm py-3">{item.preparing_time} minutes</td>
+
+      <td className="text-sm py-3">{item.calories} calories</td>
+
+      <td className="py-3">
+        <button className="bg-green-600 text-black text-sm px-4 py-1 rounded-full">
+          Preparing
+        </button>
+      </td>
+    </tr>
+  );
 };
 
 CookItem.propTypes = {
